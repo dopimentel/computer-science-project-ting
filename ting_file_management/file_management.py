4231bc8c1f8d3c4fe5bf79243966cf3144c83898
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-19 11:25:03 -0300

    file_management.py implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.lower().endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+
+        with open(path_file, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    print(txt_importer("statics/arquivo_teste.txt"))
