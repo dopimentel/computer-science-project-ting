79e65040e4c0c06850383ca82542859608cf6e7a
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-19 23:45:44 -0300

    word_search.py def search_by_word implemented

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,6 +3,13 @@ from ting_file_management.queue import Queue
 from ting_file_management.file_process import process
 
 
+def find_words_in_line(word, line, index, content=False):
+    if word.lower() in line.lower():
+        return {"linha": index + 1}
+    else:
+        return None
+
+
 def exists_word(word, instance):
 
     report = []
@@ -16,12 +23,41 @@ def exists_word(word, instance):
                 print("nao encontrado")
             # print(ocorrencias)
         if len(ocorrencias) > 0:
-            report.append({"palavra": word, "arquivo": file["nome_do_arquivo"], "ocorrencias": ocorrencias})
+            report.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
     return report
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    report = []
+    for file in instance.queue():
+        ocorrencias = []
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                ocorrencias.append(
+                    {
+                        "linha": index + 1,
+                        "conteudo": file["linhas_do_arquivo"][index],
+                    }
+                )
+                # print(f"Arquivo: {file['nome_do_arquivo']}, Linha: {index + 1}, Ocorrências: {ocorrencias}")
+            else:
+                print("nao encontrado")
+            # print(ocorrencias)
+        if len(ocorrencias) > 0:
+            report.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return report
 
 
 if __name__ == "__main__":
